b7d0af86705a6c0d119a95bd53a6818667dec62e
matsumura <matsumura@matsumura.local>
2019-05-10 14:56:14 +0900

    update

--- dataTile/convert.rb
+++ dataTile/convert.rb
@@ -1,8 +1,10 @@
 z = 0
+print "directory name:"
+dir = gets.chomp
 while z <= 4
     (2**z).times do |x|
         (2**z).times do |y|
-            system("convert #{z}/#{x}/#{y}.txt #{z}/#{x}/#{y}.png")
+            system("convert #{dir}/#{z}/#{x}/#{y}.txt #{dir}/#{z}/#{x}/#{y}.png")
             y += 1  
        end
        x += 1
