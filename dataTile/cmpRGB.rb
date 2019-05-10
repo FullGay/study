a942ec639bb1a7af223cf047214902aefaa84c58
matsumura <matsumura@matsumura.local>
2019-05-10 14:26:31 +0900

    improved

--- dataTile/cmpRGB.rb
+++ dataTile/cmpRGB.rb
@@ -10,36 +10,24 @@ while z <= maxZoom
             File.open("#{y}_pre.txt","w") do |fw|
                 fw.puts("# ImageMagick pixel enumeration: 240,240,255,srgb")
                 File.open("tmp.txt"){|f|
-                    f.gets()
-                    f.each_line{|line|
-                    if /grey|silver|DarkKhaki|turquoise|RosyBrown/=~line
-                      tmp = line.slice(line.index("#")+1,2)
-                      r = Integer("0x#{tmp}")
-                      tmp = line.slice(line.index("#")+3,2)
-                      g = Integer("0x#{tmp}")
-                      tmp = line.slice(line.index("#")+5,2)
-                      b = Integer("0x#{tmp}")
-                      line.gsub!(/grey.*|silver.*|DarkKhaki.*|turquoise.*|RosyBrown.*/,"srgb(#{r},#{g},#{b})")
-                    end
-                    #begin if /turquoise/=~line
-                    #    tmp = line.slice(line.index("#")+1,2)
-                    #    r = Integer("0x#{tmp}")
-                    #    tmp = line.slice(line.index("#")+3,2)
-                    #    g = Integer("0x#{tmp}")
-                    #    tmp = line.slice(line.index("#")+5,2)
-                    #    b = Integer("0x#{tmp}")
-                    #    line.gsub!(/turquoise./,"srgb(#{r},#{g},#{b})")
-                    #end
-                        line.slice!(/\(.*srgb/)
-                        fw.puts(line)
-                    }
+                  f.gets()
+                  f.each_line{|line|
+                    tmp = line.slice(line.index("#")+1,2)
+                    r = Integer("0x#{tmp}")
+                    tmp = line.slice(line.index("#")+3,2)
+                    g = Integer("0x#{tmp}")
+                    tmp = line.slice(line.index("#")+5,2)
+                    b = Integer("0x#{tmp}")
+                    line.gsub!(/\(.*/,"(#{r},#{g},#{b})")
+                    fw.puts(line)
+                  }
                 }
             end
             puts "diff #{z}/#{x}/#{y}.txt #{z}/#{x}/#{y}_pre.txt"
             system("diff #{y}.txt #{y}_pre.txt")
         end
         x += 1
-        system("rm tmp.txt")
+        #system("rm tmp.txt")
         Dir::chdir("..")
     end
     z += 1
